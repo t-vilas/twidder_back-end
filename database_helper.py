f9fe6e8d850ad2fea39c9b1e9e640bc70b5e3160
dyavil <dyavil.q@gmail.com>
2015-02-19 10:50:55 +0100

    final py functions

--- database_helper.py
+++ database_helper.py
@@ -22,20 +22,29 @@ def get_user(email, password, token):
 	print "db1"
 	cursor = c.cursor()
 	print email
+	print token
 	cursor.execute("UPDATE users SET token = ? WHERE email = ? AND password = ?", (token, email, password))
 	cursor.execute("SELECT * FROM users WHERE email = ? AND password = ?", (email, password))
 	print password
-	result = [dict(id=row[0], email=row[1], firstname=row[3], familyname=row[4], gender=row[5], city=row[6], country=row[7], token=row[8]) for row in cursor.fetchall()]
+	c.commit()
+	result = [dict(email=row[0], firstname=row[2], familyname=row[3], gender=row[4], city=row[5], country=row[6], token=row[7]) for row in cursor.fetchall()]
 	print result
-	cursor.close()
 	print "trtrtrtrt"
+	if cursor.execute("SELECT Count(*) FROM users WHERE email = ? AND password = ?", (email, password)).fetchone()[0] <= 0:
+		cursor.close()
+		return None
+	cursor.close()
 	return json.dumps(result[0])
 
 def get_user_info(token):
 	c = get_db()
 	cursor = c.cursor()
 	cursor.execute("SELECT * FROM users WHERE token = ?", (token, ))
-	result = [dict(email=row[1], firstname=row[3], familyname=row[4], gender=row[5], city=row[6], country=row[7]) for row in cursor.fetchall()]
+	result = [dict(email=row[0], firstname=row[2], familyname=row[3], gender=row[4], city=row[5], country=row[6]) for row in cursor.fetchall()]
+	print result
+	if cursor.execute("SELECT Count(*) FROM users WHERE token = ?", (token, )).fetchone()[0] <= 0:
+		cursor.close()
+		return None
 	cursor.close()
 	return json.dumps(result[0])
 
@@ -43,27 +52,40 @@ def get_user_info_email(email):
 	c = get_db()
 	cursor = c.cursor()
 	cursor.execute("SELECT * FROM users WHERE email = ?", (email, ))
-	result = [dict(email=row[1], firstname=row[3], familyname=row[4], gender=row[5], city=row[6], country=row[7]) for row in cursor.fetchall()]
+	result = [dict(email=row[0], firstname=row[2], familyname=row[3], gender=row[4], city=row[5], country=row[6]) for row in cursor.fetchall()]
+	print result
+	if cursor.execute("SELECT Count(*) FROM users WHERE email = ?", (email, )).fetchone()[0] <= 0:
+		cursor.close()
+		return None
 	cursor.close()
 	return json.dumps(result[0])
 
 def get_user_messages(token):
 	c = get_db()
 	cursor=c.cursor()
-	cursor.execute("SELECT id FROM users WHERE token = ?", (token, ))
-	userid = cursor.fetchone()
-	cursor.execute("SELECT * FROM messages WHERE writer = ? OR receiver = ?", (userid, userid))
+	cursor.execute("SELECT email FROM users WHERE token = ?", (token, ))
+	useremail = cursor.fetchone()
+	useremail = useremail[0]
+	print useremail
+	cursor.execute("SELECT * FROM messages WHERE writer = ? OR receiver = ? ORDER BY id DESC", (useremail, useremail))
 	result = [dict(writer=row[1], content=row[2], receiver=row[3]) for row in cursor.fetchall()]
+	
+	if (cursor.execute("SELECT Count(*) FROM messages WHERE writer = ? OR receiver = ?", (useremail, useremail)).fetchone()[0]) <= 0:
+		cursor.close()
+		print "bla"
+		return None
 	cursor.close()
 	return json.dumps(result)
 
 def get_user_messages_email(email):
 	c = get_db()
 	cursor=c.cursor()
-	cursor.execute("SELECT id FROM users WHERE email = ?", (email, ))
-	userid = cursor.fetchone()
-	cursor.execute("SELECT * FROM messages WHERE writer = ? OR receiver = ?", (userid, userid))
+	cursor.execute("SELECT * FROM messages WHERE writer = ? OR receiver = ? ORDER BY id DESC", (email, email))
 	result = [dict(writer=row[1], content=row[2], receiver=row[3]) for row in cursor.fetchall()]
+	if cursor.execute("SELECT Count(*) FROM messages WHERE writer = ? OR receiver = ?", (email, email)).fetchone()[0] <= 0:
+		cursor.close()
+		print "bla"
+		return None
 	cursor.close()
 	return json.dumps(result)
 
@@ -72,29 +94,36 @@ def insert_user(email, password, firstname, familyname, gender, city, country, t
 	c = get_db()
 	cursor = c.cursor()
 	cursor.execute("INSERT INTO users (email, password, firstname, familyname, gender, city, country, token) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (email, password, firstname, familyname, gender, city, country, token))
-	result = [dict(id=row[0], email=row[1], firstname=row[3], familyname=row[4], gender=row[5], city=row[6], country=row[7], token=row[8]) for row in cursor.fetchall()]
+	result = [dict(email=row[0], firstname=row[2], familyname=row[3], gender=row[4], city=row[5], country=row[6], token=row[7]) for row in cursor.fetchall()]
+	c.commit()
+	if cursor.rowcount <= 0:
+		cursor.close()
+		return None
 	cursor.close()
-	return json.dumps(result[0])
+	return json.dumps({"success" :True})
 
-def sign_out(token):
+def token0(token):
 	c = get_db()
 	cursor = c.cursor()
 	cursor.execute("UPDATE users SET token = '' WHERE token = ?", (token, ))
 	cursor.close()
+	c.commit()
 
 def change_password(token, old_password, new_password):
 	c =get_db()
 	cursor = c.cursor()
 	cursor.execute("UPDATE users SET password = ? WHERE token = ? and password = ?", (new_password, token, old_password))
 	cursor.close()
+	c.commit()
 
 def post_message(token, message, email):
 	c = get_db()
 	cursor=c.cursor()
-	cursor.execute("SELECT id FROM users WHERE token = ?", (token, ))
-	userid = cursor.fetchone()
-	cursor.execute("INSERT INTO messages (writer, content, receiver) VALUES (?, ?, ?)", (userid, message, email))
-	#TODO change db : email = primary key
+	cursor.execute("SELECT email FROM users WHERE token = ?", (token, ))
+	useremail = cursor.fetchone()
+	print useremail
+	cursor.execute("INSERT INTO messages (writer, content, receiver) VALUES (?, ?, ?)", (useremail[0], message, email))
+	c.commit()
 	pass
 
 def db_close():
--- server.py
+++ server.py
@@ -14,73 +14,113 @@ app = Flask(__name__)
 def home():
 	return render_template('client.html')
 
-
-@app.route('/signin', methods=['POST'])
-def sign_in():
-	if request.method == 'POST':
-		email = request.form["siemail"]
-		print "test"
-		password = request.form["sipassword"]
-		token = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(40))
-		print password
-		m = hashlib.md5()
-		m.update(password)
-		print m.hexdigest()
-		pwd = m.hexdigest()
-		result = database_helper.get_user(email, pwd, token)
-		print "test3"
-		return render_template("client.html", token=token)
+@app.route('/signin/<email>/<password>', methods=['POST'])
+def sign_in(email, password):
+	token = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(40))
+	print password
+	m = hashlib.md5()
+	m.update(password)
+	print m.hexdigest()
+	pwd = m.hexdigest()
+	result = database_helper.get_user(email, pwd, token)
+	jsonfile = json.dumps({"success": True, "Message": "Log in", "data": token})
+	return jsonfile
 
 
+@app.route('/signup/<email>/<password>/<firstname>/<familyname>/<gender>/<city>/<country>', methods=['POST'])
 def sign_up(email, password, firstname, familyname, gender, city, country):
 	if request.method == 'POST':
-		token = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(40))
-		m = hashlib.md5()
-		m.update(password)
-		pwd = m.hexdigest()
-		res = database_helper.insert_user(email, pwd, firstname, familyname, gender, city, country, token)
-		print res
+		if database_helper.get_user_info_email(email) == None:
+			token = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(40))
+			m = hashlib.md5()
+			m.update(password)
+			pwd = m.hexdigest()
+			res = database_helper.insert_user(email, pwd, firstname, familyname, gender, city, country, token)
+			print res
+			jsonfile = json.dumps({"success": True, "Message": "New user created"})
+		else : jsonfile = json.dumps({"success": False, "Message": "Already use email"})
+		return jsonfile
 
+@app.route('/signout/<token>', methods=['POST'])
 def sign_out(token):
 	if request.method == 'POST':
-		database.sign_out(token);
+		return json.dumps({"success": True, "Message": "Log out !"})
 
+@app.route('/passchange/<token>/<old_password>/<new_password>', methods=['POST'])
 def change_password(token, old_password, new_password):
 	if request.method == 'POST':
 		m = hashlib.md5()
+		m2 = hashlib.md5()
 		m.update(old_password)
 		pwd1 = m.hexdigest()
-		m.update(new_password)
-		pwd2 = m.hexdigest()
+		m2.update(new_password)
+		pwd2 = m2.hexdigest()
 		database_helper.change_password(token, pwd1, pwd2)
+		jsonfile = json.dumps({"success": True, "Message": "Password changed"})
+		return jsonfile
 
+
+@app.route('/userdatatoken/<token>', methods=['GET'])
 def get_user_data_by_token(token):
 	res = database_helper.get_user_info(token)
-	return res
+	if res != None:
+		jsonfile = json.dumps({"success": True, "Message": "User found", "Data": res})
+	else : jsonfile = json.dumps({"success": False, "Message": "Token error"})
+	return jsonfile
 
+@app.route('/userdataemail/<token>/<email>')
 def get_user_data_by_email(token, email):
 	if get_user_data_by_token(token) != None:
 		res = database_helper.get_user_info_email(email)
-	return res
+		if res != None:
+			jsonfile = json.dumps({"success": True, "Message": "User found", "Data": res})
+		else : jsonfile = json.dumps({"success": False, "Message": "Email do not exist"})
+	else : jsonfile = json.dumps({"success": False, "Message": "Token error"})
+	return jsonfile
 
+@app.route('/messages/<token>')
 def get_user_messages_by_token(token):
-		messages = database_helper.get_user_messages(token)
-		print messages
-	return messages
+	messages = database_helper.get_user_messages(token)
+	if messages != None:
+		jsonfile = json.dumps({"success": True, "Message": "messages found", "Data": messages})
+	else : jsonfile = json.dumps({"success": False, "Message": "no messages"})
+	return jsonfile
 
+@app.route('/messages/<token>/<email>')
 def get_user_messages_by_email(token, email):
 	if get_user_data_by_token(token) != None:
 		messages = database_helper.get_user_messages_email(email)
-		print messages
-		return messages
+		if messages != None:
+			jsonfile = json.dumps({"success": True, "Message": "messages found", "Data": messages})
+		else : jsonfile = json.dumps({"success": False, "Message": "no messages"})
+	else : jsonfile = json.dumps({"success": False, "Message": "token error"})
+	return jsonfile
 
+@app.route('/postmessage/<token>/<message>/<email>', methods=['POST'])
 def post_message(token, message, email):
 	if get_user_data_by_token(token) != None:
+		message = html_escape(message)
+		print message
 		database_helper.post_message(token, message, email)
+		jsonfile = json.dumps({"success": True, "Message": "Message send"})
+	else : jsonfile = json.dumps({"success": False, "Message": "token error"})
+	return jsonfile
 
 @app.route('/hello')
 def hello_world():
     return 'Hello Flask!'
+
+html_escape_table = {
+    "&": "&amp;",
+    '"': "&quot;",
+    "'": "&apos;",
+    ">": "&gt;",
+    "<": "&lt;",
+}
+
+def html_escape(text):
+    return "".join(html_escape_table.get(c,c) for c in text)
+
 
 
 if __name__ == '__main__':
